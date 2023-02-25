215fdba8ee5e94ad7d7286e4f28574fdeea0d882
Yarden zamir <make.charcoal@gmail.com>
2023-02-25 15:37:38 +0200

    Create wrap.rb

---
+++ Formula/wrap.rb
@@ -0,0 +1,16 @@
+class Wrap < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Yarden-zamir/homebrew-wrap/archive/refs/tags/0.1.0.tar.gz"
+  sha256 "04a912d65cc8aecf8a7b542ec506e4aaf25a2b466e5d3aed21e00066ef045cbe"
+  license ""
+
+
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+  end
+
+  test do
+    system bin/"wrap", "--help"
+  end
+end
