89f5b2a54424c714ae871157ac997d48d21b2b91
Yarden zamir <make.charcoal@gmail.com>
2023-02-25 15:42:44 +0200

    Update wrap.rb

--- Formula/wrap.rb
+++ Formula/wrap.rb
@@ -4,13 +4,10 @@ class Wrap < Formula
   url "https://github.com/Yarden-zamir/homebrew-wrap/archive/refs/tags/0.1.0.tar.gz"
   sha256 "04a912d65cc8aecf8a7b542ec506e4aaf25a2b466e5d3aed21e00066ef045cbe"
   license ""
-
   depends_on "python@3.10"
-  
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
   end
-
   test do
     system bin/"wrap", "--help"
   end
