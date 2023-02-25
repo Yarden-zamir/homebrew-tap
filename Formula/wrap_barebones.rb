6f2ad6ddd1605fe9a81d87efd316205eb407b369
Yarden zamir <make.charcoal@gmail.com>
2023-02-25 16:23:44 +0200

    Fix archive

--- Formula/wrap_barebones.rb
+++ Formula/wrap_barebones.rb
@@ -1,7 +1,7 @@
 class Wrap < Formula
   desc "Generates 'wraps' for functions in any language using 'processors'"
   homepage "https://github.com/Yarden-zamir/wrap"
-  url "https://github.com/Yarden-zamir/WRAP/archive/refs/tags/0.2.0.zip"
+  url "https://github.com/Yarden-zamir/WRAP/archive/refs/tags/0.2.0.tar.gz"
   sha256 "337bbaad08f944c48f0c517de709074e302e370300f8075cd4502de1595d9275"
   license ""
   depends_on "python@3.10"
