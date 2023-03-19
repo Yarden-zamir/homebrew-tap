ef53d224a82d5461410ea83d48d529a828ed7cf8
Yarden-zamir <dev@yarden-zamir.com>
2023-03-19 07:20:17 +0000

    "Brew formula update for WRAP version v0.2.53"

--- Formula/WRAP.rb
+++ Formula/WRAP.rb
@@ -5,8 +5,8 @@
 class Wrap < Formula
   desc "Generates 'wraps' for functions in any language using 'processors'"
   homepage "https://github.com/Yarden-zamir/WRAP"
-  url "https://github.com/Yarden-zamir/WRAP/archive/v0.2.52.tar.gz"
-  sha256 "6d5c34fdda95da319980e62e91920f19e4831d40af591d717c4474106d1d140d"
+  url "https://github.com/Yarden-zamir/WRAP/archive/v0.2.53.tar.gz"
+  sha256 "665d746d2796db1ad8c5f4a3aa1cb07d96e817bfab70de095bc7d0a6b6e6f8a1"
   license "MIT"
 
   depends_on "python@3.11"
