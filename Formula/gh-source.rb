3107c747a1cc9bdad0c8a14812fbb596f229bc47
Yarden-zamir <dev@yarden-zamir.com>
2023-03-25 13:02:33 +0000

    "Brew formula update for gh-source version v0.0.5"

--- Formula/gh-source.rb
+++ Formula/gh-source.rb
@@ -5,12 +5,12 @@
 class GhSource < Formula
   desc "Plugin manager for people who don't like plugin managers"
   homepage "https://github.com/Yarden-zamir/gh-source"
-  url "https://github.com/Yarden-zamir/gh-source/archive/v0.0.4.tar.gz"
-  sha256 "846e1d82aed9136603942939854a05f92162fb789fdc288b956d9f35bd9296c7"
+  url "https://github.com/Yarden-zamir/gh-source/archive/v0.0.5.tar.gz"
+  sha256 "74e1c4c87599762910637fffe8b0d23d38d1ef48d98122fe51562ab6b8bf39e3"
   license "MIT"
 
   def install
-    libexec.install Dir["*"]
-bin.install_symlink libexec/gh-source.zsh
+    bin.install gh-source.zsh
+libexec.install Dir["*"]
   end
 end
