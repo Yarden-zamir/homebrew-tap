170464dc64c64b9db12c4b5a281b9ec14a4446de
Yarden Zamir <dev@yarden-zamir.com>
2023-05-03 09:28:08 +0300

    zsh-act-completion 0.0.4

--- Formula/zsh-act-completion.rb
+++ Formula/zsh-act-completion.rb
@@ -2,10 +2,10 @@
 # frozen_string_literal: true
 
 class ZshActCompletion < Formula
-  desc "zsh completions for act (for github)"
+  desc "Zsh completions for act (for github)"
   homepage "https://github.com/Yarden-zamir/zsh-act-completion"
-  url "https://github.com/Yarden-zamir/zsh-act-completion/archive/v0.0.3.tar.gz"
-  sha256 "f1c799408bd3f68c8cd490c932c1314c08fb86a13f0a4584c607d6338d1ccd69"
+  url "https://github.com/Yarden-zamir/zsh-act-completion/archive/v0.0.4.tar.gz"
+  sha256 "0310dc2940fdc51dc5b1c44dcfdd77b0916af1522391dfb2f1698903367c59f7"
   license "MIT"
 
   depends_on "gh"
