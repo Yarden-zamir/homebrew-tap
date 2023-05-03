307775c38d40f432c6338b8f8bfcacaf8ddaf8ed
runner <runner@Mac-1683113702985.local>
2023-05-03 11:37:51 +0000

    zsh-act-completion 0.0.19

--- Formula/zsh-act-completion.rb
+++ Formula/zsh-act-completion.rb
@@ -4,8 +4,8 @@
 class ZshActCompletion < Formula
   desc "Zsh completions for act (for github)"
   homepage "https://github.com/Yarden-zamir/zsh-act-completion"
-  url "https://github.com/Yarden-zamir/zsh-act-completion/archive/v0.0.8.tar.gz"
-  sha256 "1897e31d7a516a159ec32a04bd7bef97250ca04503e6c24400c2d91e4913ce63"
+  url "https://github.com/Yarden-zamir/zsh-act-completion/archive/v0.0.19.tar.gz"
+  sha256 "410090b40778f681d1d0f7767e5d8be83fd3f47f50cbc021757486bf958bce12"
   license "MIT"
 
   def install
