1a7defd4854a71fdcf19097d96b812b799863d1a
Yarden Zamir <dev@yarden-zamir.com>
2023-05-03 08:46:49 +0300

    Add zsh-act-completion

---
+++ Formula/zsh-act-completion.rb
@@ -0,0 +1,23 @@
+# typed: false
+# frozen_string_literal: true
+
+class ZshActCompletion < Formula
+  desc "zsh completions for act (for github)"
+  homepage "https://github.com/Yarden-zamir/zsh-act-completion"
+  url "https://github.com/Yarden-zamir/zsh-act-completion/archive/v0.0.21.tar.gz"
+  sha256 "f1c799408bd3f68c8cd490c932c1314c08fb86a13f0a4584c607d6338d1ccd69"
+  license "MIT"
+
+  depends_on "gh"
+
+  def install
+    zsh_completion.install "_act"
+  end
+
+  def caveats
+    <<~EOS
+      zsh completions have been installed to:
+        /opt/homebrew/share/zsh/site-functions
+    EOS
+  end
+end
